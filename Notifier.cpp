1331c506cef4e116518281beaeb73a46e9483e1f
yudonguk <yudonguk@naver.com>
2013-07-19 00:09:58 +0900

    다른 스레드로 작업 완료를 알리는 Notifier 작성
    1. 스레드들이 하나의 작업이 완료될 때 까지 대기하다가, 작업이 완료되면 스레드가 동작하게하여 작업의 완료를 다른 스레드로 알리는 기능을 갖는 Notifier 작성

---
+++ Notifier.cpp
@@ -0,0 +1,112 @@
+﻿#include "Notifier.h"
+
+std::unique_ptr<std::deque<std::shared_ptr<Notifier>>, std::function<void(std::deque<std::shared_ptr<Notifier>>*)>> Notifier::mpPool;
+boost::shared_mutex Notifier::mPoolMutex;
+
+Notifier::Notifier()
+	: mStatus(WAIT)
+{
+}
+
+Notifier::~Notifier()
+{
+	Reset();
+}
+
+std::shared_ptr<Notifier> Notifier::Allocate()
+{
+	boost::upgrade_lock<boost::shared_mutex> upgradeLock(mPoolMutex);
+	if (mpPool.get() == nullptr)
+	{
+		boost::upgrade_to_unique_lock<boost::shared_mutex> uniqueLock(upgradeLock);
+		mpPool = std::unique_ptr<std::deque<std::shared_ptr<Notifier>>
+			, std::function<void(std::deque<std::shared_ptr<Notifier>>*)>>(
+			new std::deque<std::shared_ptr<Notifier>>, [](std::deque<std::shared_ptr<Notifier>>* pPool)
+			{
+				mPoolMutex.lock();
+				auto holder = std::move(*pPool);
+				delete mpPool.release();	
+				mPoolMutex.unlock();	
+			}
+		);
+	}
+
+	std::shared_ptr<Notifier> result;
+
+	if (mpPool->empty())
+	{
+		result.reset(new Notifier, &Release);
+	}
+	else
+	{
+		boost::upgrade_to_unique_lock<boost::shared_mutex> uniqueLock(upgradeLock);
+		result.swap(mpPool->front());
+		mpPool->pop_front();
+	}
+
+	return result;
+}
+
+void Notifier::Release( Notifier* pNotifier )
+{
+	boost::upgrade_lock<boost::shared_mutex> upgradeLock(mPoolMutex);
+	if (mpPool.get() == nullptr || mpPool->size() >= 10)
+	{
+		delete pNotifier;
+	}
+	else
+	{
+		pNotifier->Reset();
+	
+		boost::upgrade_to_unique_lock<boost::shared_mutex> uniqueLock(upgradeLock);
+		mpPool->push_back(std::shared_ptr<Notifier>(pNotifier, &Release));
+	}
+}
+
+bool Notifier::IsCanceled()
+{
+	boost::mutex::scoped_lock lock(mMutex);
+	return mStatus != CANCEL;
+}
+
+void Notifier::Notify()
+{
+	boost::mutex::scoped_lock lock(mMutex);
+	if (mStatus != WAIT)
+		return;
+
+	mStatus = COMPLETE;
+	mConditionVariable.notify_all();
+}
+
+void Notifier::Cancel()
+{
+	boost::mutex::scoped_lock lock(mMutex);
+	if (mStatus != WAIT)
+		return;
+
+	mStatus = CANCEL;
+	mConditionVariable.notify_all();
+}
+
+void Notifier::Reset()
+{
+	boost::mutex::scoped_lock lock(mMutex);
+	mStatus = WAIT;
+	mConditionVariable.notify_all();
+}
+
+bool Notifier::Wait( uint32_t timeout /*= 0xFFFFFFFF*/ )
+{
+	boost::mutex::scoped_lock lock(mMutex);
+	if (mStatus == COMPLETE)
+		return true;
+	else if(mStatus == CANCEL)
+		return false;
+
+	if (mConditionVariable.wait_for(lock, boost::chrono::milliseconds(timeout))
+		== boost::cv_status::timeout || mStatus != COMPLETE)
+		return false;
+	
+	return true;
+}
---
+++ Notifier.h
@@ -0,0 +1,51 @@
+﻿#ifndef __NOTIFIER_H__
+#define __NOTIFIER_H__
+
+#include <memory>
+#include <cstdint>
+#include <deque>
+#include <functional>
+
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
+
+class Notifier
+{
+private:
+	enum Status	{ WAIT, CANCEL, COMPLETE };
+
+private:
+	Notifier();
+
+public:
+	~Notifier();
+
+private:
+	// 복사 생성자 및 연산자 제거
+	Notifier(const Notifier&);
+	const Notifier& operator=(const Notifier&);
+
+public:
+	static std::shared_ptr<Notifier> Allocate();
+
+public:
+	bool IsCanceled();
+	void Notify();
+	void Cancel();
+	void Reset();
+	bool Wait(uint32_t timeout = 0xFFFFFFFF);
+
+private:
+	static void Release(Notifier* pNotifier);
+
+private:
+	static std::unique_ptr<std::deque<std::shared_ptr<Notifier>>, std::function<void(std::deque<std::shared_ptr<Notifier>>*)>> mpPool;
+	static boost::shared_mutex mPoolMutex;
+
+private:
+	boost::mutex mMutex;
+	boost::condition_variable mConditionVariable;
+	Status mStatus;
+};
+
+#endif // !__NOTIFIER_H__
